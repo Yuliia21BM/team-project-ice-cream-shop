f93c63704f90827f5f741bca59c0933de8bcc677
Sergey <sergey.cherenkov96@gmail.com>
2022-09-17 19:53:01 +0300

    features/css-tablet-desktop-customers

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,22 +1,12 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
+  spaceBetween: 100,
   loop: true,
 
   // If we need pagination
   pagination: {
       el: '.swiper-pagination',
       clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
