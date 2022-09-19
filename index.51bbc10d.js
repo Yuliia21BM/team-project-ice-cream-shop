b1cff3274d99b049efd3f94aefbf9796993d03ad
vladdd1792 <107995468+vladdd1792@users.noreply.github.com>
2022-09-19 23:03:28 +0000

    Deploying to gh-pages from @ Yuliia21BM/team-project-ice-cream-shop@725edf8e8eef2a059c456cc6c0e4359d9792f2a7 🚀

--- index.51bbc10d.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-ingrid-open-1]"),closeModalBtn:document.querySelector("[data-modal-ingrid-close-1]"),modal:document.querySelector("[data-modal-ingrid-1]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.51bbc10d.js.map
--- index.ca7fd949.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-ingrid-open-2]"),closeModalBtn:document.querySelector("[data-modal-ingrid-close-2]"),modal:document.querySelector("[data-modal-ingrid-2]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.ca7fd949.js.map
