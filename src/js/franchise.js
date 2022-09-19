30863fabba1659cec6b1ce9e82e9b084b18fa7a2
VladVeselov <vlaq1997@gmail.com>
2022-09-19 13:54:21 +0300

    js

--- src/js/franchise.js
+++ src/js/franchise.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-      // openModalBtn: document.querySelector('[data-modal-ingrid-open]'),
+      // openModalBtn: document.querySelector('[data-modal-franchise-open]'),
       closeModalBtn: document.querySelector('[data-modal-franchise-close]'),
-      modal: document.querySelector('[data-modal-ingrid]'),
+      modal: document.querySelector('[data-modal-franchise]'),
       body: document.querySelector('body'),
     };
     //   refs.openModalBtn.addEventListener('click', toggleModal);
