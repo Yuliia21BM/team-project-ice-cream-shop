144c235831995b68037f064504e06f6b9a493be6
Yuliia21BM <108929319+Yuliia21BM@users.noreply.github.com>
2022-09-22 07:52:06 +0000

    Deploying to gh-pages from @ Yuliia21BM/team-project-ice-cream-shop@c7634a7368e995bc2f4bbc295deb4c3e2863764d 🚀

--- index.597992ce.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-ingrid-open]"),closeModalBtn:document.querySelector("[data-modal-ingrid-close]"),modal:document.querySelector("[data-modal-ingrid]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.597992ce.js.map
