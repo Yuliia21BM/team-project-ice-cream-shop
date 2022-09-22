0639f4ab5d447545faf9a9ed2437ed0d5ffe039f
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2022-09-22 13:33:04 +0300

    Add

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './js/modal';
 import './js/about-modal';
 import './js/franchise';
 import './js/modal-mob';
+import './js/ingrid-modal';
 import './js/ingrid-modal-1';
 import './js/ingrid-modal-2';
 import './js/location-modal';
