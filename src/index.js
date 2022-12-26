f6d5356075b633d9098c0b62b4c8fe6261413b16
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2022-12-26 22:10:39 +0200

    Add smooth_scroll

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ import './js/ingrid-modal';
 import './js/ingrid-modal-1';
 import './js/ingrid-modal-2';
 import './js/location-modal';
-import './js/jquery-3.2.1.min.js'
-import './js/jquery.spincrement.min.js'
+import './js/jquery-3.2.1.min.js';
+import './js/jquery.spincrement.min.js';
